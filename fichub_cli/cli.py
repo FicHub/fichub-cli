483db232e7bbf94966c6dec0ae0e2fa5947ff6a7
arzkar <arzkar.dev@gmail.com>
2021-06-14 00:56:22 +0530

    Fixed a few bugs, change the progress bar's format & updated the logging (#7)
    - Bugfix & updated the logging
        - utils/fetch_data.py:
            - A bug where if the fanfiction was not found,
          it wasn't skipping it and instead raising an error
    
        - cli.py: A bug where it was raising an error when
              `-v` & `-s` flags were used because fic.exit_status
              didnt exist in these cases and the cli was trying to
              sys.exit(fic.exit_status)
    
    - Progressbar:
        - Replaced the unit for progressbar from 'it' to 'file'
        - Changed the progressbar format and added an ETA
    
    -  Created FicHub & FetchData classes

--- fichub_cli/cli.py
+++ fichub_cli/cli.py
@@ -1,14 +1,15 @@
 import click
 import sys
 from loguru import logger
+from datetime import datetime
 
-from .utils.get_fic import get_fic_with_infile, get_fic_with_list, \
-    get_fic_with_url, get_urls_from_page
-
+from .utils.fetch_data import FetchData
 from .utils.processing import get_format_type
 
+time = datetime.now().strftime("%H_%M_%S")
 
-# @logger.catch
+
+# @logger.catch  # for internal debugging
 @click.command(no_args_is_help=True)
 @click.option('-u', '--url', help='The url of the fanfiction enclosed within quotes ')
 @click.option('-i', '--infile', help='Give a filename to read URLs from')
@@ -28,34 +29,38 @@ def run_cli(infile: str, url: str, list_url: str, _format: str, get_urls: str,
     """
     A CLI for the fichub.net API
 
-    To report issues upstream for supported sites, visit https://fichub.net/#contact 
+    To report issues upstream for supported sites, visit https://fichub.net/#contact
 
     To report issues for the CLI, open an issue at https://github.com/FicHub/fichub-cli/issues
     """
 
     if log:
         debug = True
-        logger.add("fichub_cli.log")
+        logger.add(f"fichub_cli_{time}.log")
 
-    exit_status = 0
     format_type = get_format_type(_format)
     if infile:
-        exit_status = get_fic_with_infile(
-            infile, format_type, out_dir, debug, force, automated)
+        fic = FetchData(format_type, out_dir, force,
+                        debug, automated)
+        fic.get_fic_with_infile(infile)
 
     elif list_url:
-        exit_status = get_fic_with_list(
-            list_url, format_type, out_dir, debug, force, automated)
+        fic = FetchData(format_type, out_dir, force,
+                        debug, automated)
+        fic.get_fic_with_list(list_url)
 
     elif url:
-        exit_status = get_fic_with_url(
-            url, format_type, out_dir, debug, force, automated)
+        fic = FetchData(format_type, out_dir, force,
+                        debug, automated)
+        fic.get_fic_with_url(url)
 
     elif get_urls:
-        exit_status = get_urls_from_page(get_urls, debug, automated)
+        fic = FetchData(debug)
+        fic.get_urls_from_page(get_urls)
 
     if version:
-        click.echo("Version: 0.3.4a")
+        click.echo("Version: 0.3.4b")
+        sys.exit(0)
 
     if supported_sites:
         click.echo("""
@@ -79,10 +84,8 @@ def run_cli(infile: str, url: str, list_url: str, _format: str, get_urls: str,
         - FanficAuthors (minimal)
         - Harry Potter Fanfiction (archive from pre-revival)
 
-    To report issues upstream for these sites, visit https://fichub.net/#contact     
+    To report issues upstream for these sites, visit https://fichub.net/#contact
 """)
+        sys.exit(0)
 
-    if log:  # To separate each run
-        logger.info(f"{20*'-'}EXITING{20*'-'}")
-
-    sys.exit(exit_status)
+    sys.exit(fic.exit_status)
