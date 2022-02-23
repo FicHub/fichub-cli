f4b9c9c858a241e09edf09debc9f9f39ffb1de83
arzkar <arzkar.dev@gmail.com>
2022-02-23 14:29:39 +0530

    Bump version: 0.5.4 → 0.6.0
    - Refactored code to make it compatible with the plugins
    
    - Moved `--get-urls` option to the `fichub-cli-metadata` plugin
    
    - Added output.log for downloads tracking
    
    - Logging to file only when using `--log`

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -61,4 +61,4 @@ def test_cli_version():
 
     assert not result.exception
     assert result.exit_code == 0
-    assert result.output.strip() == 'fichub-cli: v0.5.4'
+    assert result.output.strip() == 'fichub-cli: v0.6.0'
