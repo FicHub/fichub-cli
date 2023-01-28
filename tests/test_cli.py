9c5ba32a0f82dc25bfe019772dcfbc8c49332aab
Arbaaz Laskar <arzkar.dev@gmail.com>
2023-01-28 14:45:38 +0530

    Bump version: 0.8.4 → 0.9.0

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -61,4 +61,4 @@ def test_cli_version():
 
     assert not result.exception
     assert result.exit_code == 0
-    assert result.output.strip() == 'fichub-cli: v0.8.4'
+    assert result.output.strip() == 'fichub-cli: v0.9.0'
